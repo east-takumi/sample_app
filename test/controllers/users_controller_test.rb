fb5ee100d6b3af9d5ea1a429ad0923e13fe0cf1f
east-takumi <spielwiese82945@gmail.com>
2021-02-14 07:09:00 +0000

    complite 10.4.1

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,5 +43,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+  
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)                                                    # @other_userでログインする
+    assert_not @other_user.admin?                                             # @toher_userが管理権限あれば(adminがtrueなら)falseを返す
+    patch user_path(@other_user), params: {                                   # /users/@other_user へparamsハッシュの中身を送る
+                                  user: { password:               'password',
+                                          password_confirmation:  'password',
+                                          admin: true } }
+    assert_not @other_user.reload.admin?                                      # @other_userを再読み込みし、admin論理値が変更されてないか検証(falseやnilならtrue)
+  end
 
 end
