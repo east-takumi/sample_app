a9676edf41695ff5f6673fa3599382475d6598c4
east-takumi <spielwiese82945@gmail.com>
2021-02-14 07:32:57 +0000

    Finish user edit, update, index, and destroy actions

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -53,5 +53,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                           admin: true } }
     assert_not @other_user.reload.admin?                                      # @other_userを再読み込みし、admin論理値が変更されてないか検証(falseやnilならtrue)
   end
-
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
